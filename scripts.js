f9ea92fbb1232cc457924494d225e3b92e94a479
Bkotay <bkotay4@gmail.com>
2024-11-16 23:37:26 -0500

    Home, Nav, footer edited

--- scripts.js
+++ scripts.js
@@ -1,17 +1,21 @@
+const productGrid = document.querySelector('.container')
 fetch('user.json') // fetching products in json file //.then speeds up the process to fetch the project
 .then(response => response.json())
 .then(json => {
     const products = json;
-    return products
+    for(let i = 0; i < 12; i++) {
+        const allProducts = document.createElement('product-section');
+        productGrid.appendChild(allProducts);
+    
+    
+        allProducts.innerHTML = `
+        <img src=${products[i].location}>
+        <p class="name">${products[i].name}</p>
+        <p class="price">${products[i].price}</p>
+        <p class="desc">${products[i].desc}</p>
+        `
+    }
 })
 .catch(error => console.log(error))
 
-const productGrid = document.querySelector('.container')
 
-for(let i = 0; i < 12; i++) {
-    const allProducts = document.createElement('div');
-    productGrid.appendChild(allProducts);
-
-
-    allProducts[i].textContent = products.name;
-}
